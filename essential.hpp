ae66d63a3bdd28b20abf2d3210d06a9131bc5359
gongmg <rhdalsrn2@naver.com>
2024-06-06 14:16:29 +0900

    [Add,Mod] privateMessge 함수 추가, handleResponse 함수 인자 수정

--- essential.hpp
+++ essential.hpp
@@ -47,4 +47,7 @@ extern std::string server_name;
 #define RPL_NOTOPIC           331
 #define RPL_TOPIC             332
 
-const std::string& handleResponse(Client *client, int responseCode, const std::string& target = "", const std::string& additionalInfo = "");
+const std::string& handleResponse(const std::string& nickname, int responseCode, \
+		const std::string& target = "", const std::string& additionalInfo = "");
+const std::string& privateMessage(const std::string& nickname, \
+		const std::string& channel_name, const std::string& message);
